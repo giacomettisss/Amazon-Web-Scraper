25c8695d6dbb44e024825a9d92a867c5ad299ab0
Marcelo Giacometti da Silva <marcelogiacometti@hotmail.com>
2024-01-27 18:58:21 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,18 +1,18 @@
 # settings.py
 
 
-# db_config = {
-#     'host': 'db',
-#     'user': 'root',
-#     'password': 'example',
-#     'port': 3306,
-#     'database': 'amazon_books_db'
-# }
-
 db_config = {
-    'host': 'localhost',
+    'host': 'db',
     'user': 'root',
     'password': 'example',
-    'port': 3307,
+    'port': 3306,
     'database': 'amazon_books_db'
 }
+
+# db_config = {
+#     'host': 'localhost',
+#     'user': 'root',
+#     'password': 'example',
+#     'port': 3307,
+#     'database': 'amazon_books_db'
+# }
